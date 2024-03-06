7ff5b092e83483e0d9fb2ff3a2ec9e73222c8100
rahg-odoo <rahg@odoo.com>
2024-03-06 10:51:27 +0530

    minor changes

--- stock_transport/models/fleet_vehicle_model_category.py
+++ stock_transport/models/fleet_vehicle_model_category.py
@@ -4,8 +4,8 @@ from odoo import fields, api, models
 class FleetVehicleModelCategory(models.Model):
     _inherit = "fleet.vehicle.model.category"
 
-    max_weight = fields.Float(string='Max Weight(kg)',default = 10)
-    max_volume = fields.Float(string='Max Volume(m3)', default =10)
+    max_weight = fields.Float(string='Max Weight(kg)', default=10)
+    max_volume = fields.Float(string='Max Volume(m3)', default=10)
 
     @api.depends('max_weight', 'max_volume')
     def _compute_display_name(self):
--- stock_transport/models/stock_picking.py
+++ stock_transport/models/stock_picking.py
@@ -9,14 +9,8 @@ class StockPicking(models.Model):
 
     def _compute_volume(self):
         for record in self:
-            record.volume = 0
-            for product in record.move_ids:
-                record.volume += product.product_id.volume * product.quantity
+            record.volume = sum(record.move_ids.mapped('product_id.volume'))
 
     def _compute_weight(self):
         for record in self:
-            record.weight = 0
-            for product in record.move_ids:
-                record.weight += product.product_id.weight * product.quantity
-
-
+            record.weight = sum(record.move_ids.mapped('product_id.weight'))
